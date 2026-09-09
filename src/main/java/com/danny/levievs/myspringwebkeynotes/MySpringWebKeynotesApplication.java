06aae37f5597528221f931d0895d1a8f116f64c8
DannyLeviev <danny.leviev@gmail.com>
2026-09-09 12:35:14 -0700

    Proj init

---
+++ src/main/java/com/danny/levievs/myspringwebkeynotes/MySpringWebKeynotesApplication.java
@@ -0,0 +1,13 @@
+package com.danny.levievs.myspringwebkeynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringWebKeynotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringWebKeynotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danny/levievs/myspringwebkeynotes/MySpringWebKeynotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danny.levievs.myspringwebkeynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringWebKeynotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
